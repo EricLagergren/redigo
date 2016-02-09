d57c62104020571d5e7bd5c07e95dd6fc0e46043
Eric Lagergren <ericscottlagergren@gmail.com>
2016-02-09 15:48:34 -0800

    Fix for github.com/garyburd/redigo/issues/77

--- redis/server_commands.go
+++ redis/server_commands.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"bytes"
 	"errors"
+	"fmt"
 	"reflect"
 	"strconv"
 	"strings"
 )
 
+// Info holds the data returned from redis' INFO command.
 type Info struct {
 	Server      ServerInfo
 	Clients     ClientsInfo
@@ -21,6 +23,8 @@ type Info struct {
 	Keyspace    KeyspaceInfo
 }
 
+// ServerInfo holds the data inside the "# Server" portion
+// of redis' INFO command.
 type ServerInfo struct {
 	RedisVersion    string `redis:"redis_version"`
 	RedisGitSHA1    string `redis:"redis_git_sha1"`
@@ -41,6 +45,8 @@ type ServerInfo struct {
 	ConfigFile      string `redis:"config_file"`
 }
 
+// ClientsInfo holds the data inside the "# Clients" portion
+// of redis' INFO command.
 type ClientsInfo struct {
 	ConnectedClients        int64 `redis:"connected_clients"`
 	ClientLongestOutputList int64 `redis:"client_longest_output_list"`
@@ -48,6 +54,8 @@ type ClientsInfo struct {
 	BlockedClients          int64 `redis:"blocked_clients"`
 }
 
+// MemoryInfo holds the data inside the "# Memory" portion
+// of redis' INFO command.
 type MemoryInfo struct {
 	UsedMemory            int64   `redis:"used_memory"`
 	UsedMemoryHuman       string  `redis:"used_memory_human"`
@@ -59,6 +67,8 @@ type MemoryInfo struct {
 	MemAllocator          string  `redis:"mem_allocator"`
 }
 
+// PersistenceInfo holds the data inside the "# Persistence" portion
+// of redis' INFO command.
 type PersistenceInfo struct {
 	Loading                  bool   `redis:"loading"`
 	RDBChangesSinceLastSave  int64  `redis:"rdb_changes_since_last_save"`
@@ -76,6 +86,8 @@ type PersistenceInfo struct {
 	AOFLastWriteStatus       string `redis:"aof_last_write_status"`
 }
 
+// StatsInfo holds the data inside the "# Stats" portion
+// of redis' INFO command.
 type StatsInfo struct {
 	TotalConnectionsReceived int64   `redis:"total_connections_received"`
 	TotalCommandsProcessed   int64   `redis:"total_commands_processed"`
@@ -98,6 +110,8 @@ type StatsInfo struct {
 	MigrateCachedSockets     bool    `redis:"migrate_cached_sockets"`
 }
 
+// ReplicationInfo holds the data inside the "# Replication" portion
+// of redis' INFO command.
 type ReplicationInfo struct {
 	Role                       string `redis:"role"`
 	ConnectedSlaves            int64  `redis:"connected_slaves"`
@@ -108,6 +122,8 @@ type ReplicationInfo struct {
 	ReplBacklogHistlen         int64  `redis:"repl_backlog_histlen"`
 }
 
+// CPUInfo holds the data inside the "# CPU" portion
+// of redis' INFO command.
 type CPUInfo struct {
 	UsedCPUSys          float64 `redis:"used_cpu_sys"`
 	UsedCPUUser         float64 `redis:"used_cpu_user"`
@@ -115,16 +131,29 @@ type CPUInfo struct {
 	UsedCPUUserChildren float64 `redis:"used_cpu_user_children"`
 }
 
+// ClusterInfo holds the data inside the "# Cluster" portion
+// of redis' INFO command.
 type ClusterInfo struct {
 	ClusterEnabled bool `redis:"cluster_enabled"`
 }
 
+// KeyspaceInfo holds the data inside the "# Keyspace" portion
+// of redis' INFO command.
 type KeyspaceInfo map[string]Keyspace
 
-func (k *Keyspace) Parse(data string) error {
+// Keyspace is the data contained inside each member of the
+// "# Keyspace" section.
+type Keyspace struct {
+	Keys    int64 `redis:"keys"`
+	Expires int64 `redis:"expires"`
+	AvgTTL  int64 `redis:"avg_ttl"`
+}
 
-	if data == "" {
-		return errors.New("redigo: could not parse keyspace")
+// Set implements the Setter interface.
+func (k KeyspaceInfo) Set(key, data string) error {
+
+	if data == "" || key == "" {
+		return errors.New("redigo: could not parse keyspace because key or data was nil")
 	}
 
 	parts := strings.Split(data, ",")
@@ -133,61 +162,92 @@ func (k *Keyspace) Parse(data string) error {
 		return errors.New("redigo: could not parse keyspace")
 	}
 
-	// var ks Keyspace
-
-	vf := reflect.ValueOf(k).Elem()
-	t := vf.Type()
-	var ff reflect.Value
+	var ks Keyspace
+	kss := reflect.ValueOf(&ks).Elem()
+	t := kss.Type()
+	var field reflect.Value
 	for _, part := range parts {
 
 		kv := strings.Split(part, "=")
+		if len(kv) < 2 {
+			return errors.New("redigo: could not parse key=value pairs")
+		}
 
 		i, err := strconv.ParseInt(kv[1], 10, 64)
 		if err != nil {
 			return err
 		}
 
-		for i := 0; i < vf.NumField(); i++ {
+		for i := 0; i < kss.NumField(); i++ {
 			tag := t.Field(i).Tag.Get("redis")
 			if tag == kv[0] {
-				ff = vf.Field(i)
+				field = kss.Field(i)
 				break
 			}
 		}
 
-		ff.SetInt(i)
+		field.SetInt(i)
 	}
+	k[key] = ks
 	return nil
 }
 
-type Keyspace struct {
-	Keys    int64 `redis:"keys"`
-	Expires int64 `redis:"expires"`
-	AvgTTL  int64 `redis:"avg_ttl"`
+// Setter is the interface that the maps inside type Info should
+// implement. Setter should set the map's key with potentially parsed data.
+// This allows for better generalization of arbitrary-sized (e.g.
+// maps, slices) types.
+// For instance, the Keyspace fields are the names of each database.
+// The values of each field are comma-delimited k=v pairs. However,
+// future fields (the major "# <name>" fields) may not use k=v
+// pairs, so it makes no sense to hardcode it inside our ParseInfo
+// function.
+type Setter interface {
+	Set(key, data string) error
 }
 
-func ParseInfo(v interface{}, e error) (*Info, error) {
-	if v == nil {
-		return nil, errors.New("redigo: no data to parse")
+var setter = reflect.TypeOf((*Setter)(nil)).Elem()
+
+// ParseInfo is a helper that converts a command reply to a *Info.
+// If err is not equal to nil, then ParseInfo returns nil, err.
+// Otherwise, ParseInfo converts the reply to a *Info as follows:
+//
+// 	Reply type 	Result
+// 	string		*Info, nil
+// 	[]byte		*Info, nil
+// 	nil			nil, ErrNil
+// 	other		nil, error
+func ParseInfo(reply interface{}, err error) (*Info, error) {
+	// Return a pointer because Info is nearly 1KB in size.
+	// Future implementations will only grow the struct.
+	// I haven't profiled, but 1KB is pretty big.
+
+	if err != nil {
+		return nil, err
 	}
 
 	var s *bufio.Scanner
-	switch t := v.(type) {
+	switch reply := reply.(type) {
 	case string:
-		s = bufio.NewScanner(strings.NewReader(t))
+		s = bufio.NewScanner(strings.NewReader(reply))
 	case []byte:
-		s = bufio.NewScanner(bytes.NewReader(t))
+		s = bufio.NewScanner(bytes.NewReader(reply))
+	case nil:
+		return nil, ErrNil
+	case Error:
+		return nil, reply
 	default:
-		return nil, errors.New("redigo: cannot parse data")
+		return nil, fmt.Errorf("redigo: unexpected type for ParseInfo, got type %T", reply)
 	}
 
-	var vf, ff reflect.Value
+	// curField is the current field inside the Info struct
+	var curField reflect.Value
 	var info Info
 	val := reflect.ValueOf(&info).Elem()
 	for s.Scan() {
 		text := s.Text()
 
-		if text == "" {
+		// Skip empty lines.
+		if text == "" || text == "\n" {
 			continue
 		}
 
@@ -195,79 +255,97 @@ func ParseInfo(v interface{}, e error) (*Info, error) {
 		if strings.HasPrefix(text, "# ") {
 			parts := strings.Split(text, "# ")
 			if len(parts) < 2 {
-				return nil, errors.New("redigo: cannot parse data")
+				return nil, errors.New("redigo: line started with comment but didn't have field name")
 			}
-			vf = val.FieldByNameFunc(func(s string) bool {
-				return s == parts[1]
-			})
-			if vf.Type().Kind() == reflect.Map {
-				vf = reflect.MakeMap(vf.Type())
-				val.FieldByName(parts[1]).Set(vf)
+
+			curField = val.FieldByName(parts[1])
+			t := curField.Type()
+
+			// Initialize containers if needed.
+			if t.Kind() == reflect.Map {
+				curField.Set(reflect.MakeMap(t))
 			}
+
+			// Continue on to next subfields.
 			continue
 		}
 
 		parts := strings.Split(text, ":")
 		if len(parts) < 2 {
-			return nil, errors.New("redigo: cannot parse data")
+			return nil, errors.New("redigo: data could not be split on ':'")
 		}
 
-		t := vf.Type()
-		if t.Kind() == reflect.Struct {
-			for i := 0; i < vf.NumField(); i++ {
+		switch t := curField.Type(); t.Kind() {
+		case reflect.Struct:
+			for i := 0; i < curField.NumField(); i++ {
 				tag := t.Field(i).Tag.Get("redis")
 				if tag == parts[0] {
-					ff = vf.Field(i)
+					err := setField(curField.Field(i), parts[1])
+					if err != nil {
+						return nil, err
+					}
 					break
 				}
 			}
-		} else if t.Kind() == reflect.Map {
-			rv := reflect.New(t.Elem())
-			fn := rv.MethodByName("Parse")
-			ret := fn.Call([]reflect.Value{reflect.ValueOf(parts[1])})
-			if len(ret) != 1 {
-				return nil, errors.New("redigo: cannot parse data")
+		case reflect.Map:
+			if t.Implements(setter) {
+				fn := curField.MethodByName("Set")
+				ret := fn.Call([]reflect.Value{
+					reflect.ValueOf(parts[0]),
+					reflect.ValueOf(parts[1]),
+				})
+				if len(ret) != 1 {
+					return nil, errors.New("redigo: cannot parse data")
+				}
+				if !ret[0].IsNil() {
+					err, ok := ret[0].Interface().(error)
+					if !ok {
+						return nil, errors.New("redigo: bug where ret[0].Interface().(error) == false")
+					}
+					return nil, err
+				}
 			}
-			if !ret[0].IsNil() {
-				return nil, ret[0].Interface().(error)
-			}
-			vf.SetMapIndex(reflect.ValueOf(parts[0]), rv.Elem())
 
-			continue
 		}
 
-		switch ff.Type().Kind() {
-		case reflect.String:
-			ff.SetString(parts[1])
-		case reflect.Int, reflect.Int8,
-			reflect.Int16, reflect.Int32, reflect.Int64:
-			i, err := strconv.ParseInt(parts[1], 10, 64)
-			if err != nil {
-				return nil, err
-			}
-			ff.SetInt(i)
-		case reflect.Uint, reflect.Uint8,
-			reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			i, err := strconv.ParseUint(parts[1], 10, 64)
-			if err != nil {
-				return nil, err
-			}
-			ff.SetUint(i)
-		case reflect.Bool:
-			x, err := strconv.ParseBool(parts[1])
-			if err != nil {
-				return nil, err
-			}
-			ff.SetBool(x)
-		case reflect.Float32, reflect.Float64:
-			f, err := strconv.ParseFloat(parts[1], 64)
-			if err != nil {
-				return nil, err
-			}
-			ff.SetFloat(f)
-		default:
-			return nil, errors.New("redigo: cannot parse data")
-		}
 	}
 	return &info, s.Err()
+}
+
+func setField(field reflect.Value, val string) error {
+	switch t := field.Type().Kind(); t {
+	case reflect.String:
+		field.SetString(val)
+	case reflect.Int, reflect.Int8,
+		reflect.Int16, reflect.Int32,
+		reflect.Int64:
+		i, err := strconv.ParseInt(val, 10, 64)
+		if err != nil {
+			return err
+		}
+		field.SetInt(i)
+	case reflect.Uint, reflect.Uint8,
+		reflect.Uint16, reflect.Uint32,
+		reflect.Uint64:
+		i, err := strconv.ParseUint(val, 10, 64)
+		if err != nil {
+			return err
+		}
+		field.SetUint(i)
+	case reflect.Bool:
+		x, err := strconv.ParseBool(val)
+		if err != nil {
+			return err
+		}
+		field.SetBool(x)
+	case reflect.Float32, reflect.Float64:
+		f, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			return err
+		}
+		field.SetFloat(f)
+	default:
+		return fmt.Errorf("redigo: cannot set field of type %T", t)
+	}
+	return nil
 }
